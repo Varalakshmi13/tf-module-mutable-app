992ba09bd0ce3ee5de6a65cfb0a2a7480bc38a3e
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-29 05:14:57 +0530

    Updating tf-module-mutable-app

--- tg.tf
+++ tg.tf
@@ -15,8 +15,9 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
 
 
 resource "aws_lb_listener_rule" "app_rule" {
-  listener_arn =  data.terraform_remote_state.alb.outputs.PRIVATE_LISTNER_ARN
-  priority     = 100
+  count        = var.LB_TYPE == "internal" ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = random_integer.lb-rule_priority.result
 
   action {
     type             = "forward"
@@ -27,5 +28,23 @@ resource "aws_lb_listener_rule" "app_rule" {
     host_header {
       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"]
     }
+  }
+}
+
+
+resource "random_integer" "lb-rule_priority" {
+  min = 100
+  max = 500
+}
+
+resource "aws_lb_listener" "public_lb_listener" {
+  count             = var.LB_TYPE == "public" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
   }
 }
