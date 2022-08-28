d8e9c22e10b378f0c5e8d958eb719a2c98aa1abc
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-28 08:54:34 +0530

    Updating tf-module-mutable-app

---
+++ data.tf
@@ -0,0 +1,23 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-gvl"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-gvl"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "base-with-ansible"
+  owners      = ["self"]
+}
