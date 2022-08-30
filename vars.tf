adbabc19b77df3912dff71812b7b93ca2428caf3
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-30 13:37:39 +0530

    Updating tf-module-mutable-app

--- vars.tf
+++ vars.tf
@@ -8,8 +8,8 @@ variable "WORKSTATION_IP" {}
 variable "APP_VERSION" {}
 variable "LB_TYPE" {}
 
-variable "PUBLIC_ALB_ARN" {}
-variable "PRIVATE_ALB_ARN" {}
-variable "PRIVATE_LISTENER_ARN" {}
-variable "PRIVATE_ALB_ADDRESS" {}
-variable "PUBLIC_ALB_ADDRESS" {}
+//variable "PUBLIC_ALB_ARN" {}
+//variable "PRIVATE_ALB_ARN" {}
+//variable "PRIVATE_LISTENER_ARN" {}
+//variable "PRIVATE_ALB_ADDRESS" {}
+//variable "PUBLIC_ALB_ADDRESS" {}
